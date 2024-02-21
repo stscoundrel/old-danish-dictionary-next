5143f89c112c93d2f58ac2ec3f0382cb2915535b
Sampo Silvennoinen <silvennoinen.sampo@gmail.com>
2024-02-21 09:57:03 +0200

    Add deps & setup files

---
+++ next.config.js
@@ -0,0 +1,19 @@
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: process.env.NODE_ENV !== 'production',
+})
+
+module.exports = withPWA({
+  sassOptions: {
+    prependData: `
+        @import 'styles/variables';
+        @import 'styles/media-queries';
+        @import 'styles/rem';
+      `,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  swcMinify: true,
+})
+  
