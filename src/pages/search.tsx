d735b5d8c121a0be07e4581635dddff5bd4a4000
Sampo Silvennoinen <silvennoinen.sampo@gmail.com>
2024-02-22 19:24:05 +0200

    Add search page

---
+++ src/pages/search.tsx
@@ -0,0 +1,33 @@
+// Services.
+import { AlphabetLetter, getAlphabet } from 'lib/services/dictionary'
+
+// Components.
+import Layout from 'components/Layout'
+import SearchForm from 'components/SearchForm'
+import { ContentType } from 'lib/models/content-types'
+
+interface SearchPageProps {
+    letters: AlphabetLetter[]
+}
+
+interface SearchPageStaticProps{
+    props: SearchPageProps
+}
+
+export async function getStaticProps(): Promise<SearchPageStaticProps> {
+  const letters = getAlphabet()
+
+  return {
+    props: {
+      letters,
+    },
+  }
+}
+
+export default function Search({ letters }: SearchPageProps) {
+  return (
+    <Layout type={ContentType.Page} letters={letters} letter={null} word={null} words={[]}>
+      <SearchForm />
+    </Layout>
+  )
+}
