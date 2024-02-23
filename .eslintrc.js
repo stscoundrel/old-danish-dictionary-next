c6af226b6b89b414356e182f9df05c97e14fc544
Sampo Silvennoinen <silvennoinen.sampo@gmail.com>
2024-02-23 09:46:54 +0200

    Eslint: add required peer deps

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,5 @@
 module.exports = {
+    root: true,
     extends: [
       'airbnb-base',
       'next',
@@ -18,8 +19,6 @@ module.exports = {
       'jest/globals': true,
     },
     plugins: [
-      'import',
-      'react',
       'jest',
       '@typescript-eslint',
     ],
