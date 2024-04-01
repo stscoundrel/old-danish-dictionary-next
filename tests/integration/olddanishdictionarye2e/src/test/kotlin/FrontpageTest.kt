3fd0a266866a534e594d2963f06c15ec2e9b6e45
Sampo Silvennoinen <silvennoinen.sampo@gmail.com>
2024-04-01 21:49:07 +0300

    Add Frontpage E2E tests

---
+++ tests/integration/olddanishdictionarye2e/src/test/kotlin/FrontpageTest.kt
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class FrontpageTest : E2ETestCase() {
+    @Test
+    fun testFrontpageOpens() {
+        page?.navigate(appBaseUrl)
+    }
+
+    @Test
+    fun testFrontpageHasExpectedTitle() {
+        page?.navigate(appBaseUrl)
+        val expectedTitle = "Old Danish Dictionary - Otto Kalkar"
+        val actualTitle = page?.title()
+        assertEquals(expectedTitle, actualTitle)
+    }
+}
