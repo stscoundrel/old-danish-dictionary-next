da92dcb1f98b9a1890f549866706010d3ea53c20
Sampo Silvennoinen <silvennoinen.sampo@gmail.com>
2024-04-01 21:48:53 +0300

    Integrations tests: add scaffold and parent test case

---
+++ tests/integration/olddanishdictionarye2e/build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    kotlin("jvm") version "1.8.0"
+    application
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    implementation("com.microsoft.playwright:playwright:1.42.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.0")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(11)
+}
+
+application {
+    mainClass.set("MainKt")
+}
---
+++ tests/integration/olddanishdictionarye2e/src/main/kotlin/Main.kt
@@ -0,0 +1,9 @@
+fun main() {
+    print(
+        """
+        And if the day would only come,
+        then you might just appear,
+        even though you'd soon be gone.
+    """.trimIndent()
+    )
+}
